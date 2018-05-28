fb2f49467c4f8f25efb4a6ece4f59a1f7d7af6a8
Jorge Martí <dimartweb1@gmail.com>
2018-05-28 10:06:29 +0200

    Versión 0 - Estructura básica con flex-box

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ejemplo (1) Flexbox</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="basica.css">
+</head>
+<body>
+	<div id="template-wrapper">
+		<header id="main-header">header</header>
+		<div id="main-content"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet odio facere distinctio consequuntur laudantium quisquam, quasi porro quas, omnis animi unde veniam accusamus repellat, pariatur nesciunt. Optio sunt debitis, distinctio.</p></div>
+		<aside id="left-aside" class="main-aside">izquierda</aside>
+		<aside id="right-aside" class="main-aside">derecha</aside>
+		<footer id="main-footer">footer</footer>
+	</div>
+</body>
+</html>
